52a605d6e537a53f48b99a954340b997caf2e1d2
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2024-04-23 11:20:54 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,5 @@
-sudo apt install apache2 libapache2-mod-php php-cli php-fpm php-json php-intl php-imagick php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath mariadb-server -y
+
+sudo apt install mariadb-client mariadb-server apache2 -y
 sudo ufw allow 80
 sudo ufw allow 443
 sudo mysql_secure_installation
