3c419d70222496dc02fa37a6a728351329df663e
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2024-04-23 22:23:51 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,7 +2,7 @@ sudo add-apt-repository ppa:ondrej/php # Press enter when prompted.
 sudo apt update
 sudo apt install mariadb-client mariadb-server apache2 -y
 sudo apt install php8.2-common php8.2-cli php8.2-fpm php8.2-{curl,bz2,mbstring,intl}
-sudo apt install php8.2-cli php8.2-fpm php-json php8.2-intl php8.2-imagick php8.2-pdo php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php-pear php8.2-bcmath -y
+sudo apt install php8.2-cli php8.2-fpm php-json php8.2-intl php8.2-imagick php8.2-pdo php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php-pear php8.2-bcmath libmagickcore-6.q16-6-extra -y
 sudo apt install libapache2-mod-php
 sudo ufw allow 80
 sudo ufw allow 443
