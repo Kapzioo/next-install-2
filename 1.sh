b814bdf17ac96c440bfce6c229c1972b1eb00ef5
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2024-04-23 11:22:56 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,7 @@
-
+sudo add-apt-repository ppa:ondrej/php # Press enter when prompted.
+sudo apt update
 sudo apt install mariadb-client mariadb-server apache2 -y
+sudo apt install php8.3-common php8.3-cli php8.3-fpm php8.3-{curl,bz2,mbstring,intl}
 sudo ufw allow 80
 sudo ufw allow 443
 sudo mysql_secure_installation
